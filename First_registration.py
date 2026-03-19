4ca5e3fa6e1c381887fb3c408d40530710498439
Johns8729 <jhonsebastian8729@gmail.com>
2026-03-20 00:54:34 +0100

    Delete First_registration.py

--- First_registration.py
+++
@@ -1,9 +0,0 @@
-def customer_registration (customer_ID, client_name, email): 
-#Function created to allow the system to register the client's information
-#Parameters to show all the local variables
-    registration = {
-        "ID" : customer_ID,
-        "customer" : client_name,
-        "email" : email
-    }#Dictionary used to store all the client's data entere (Id, customer's name and email)
-    return registration 
